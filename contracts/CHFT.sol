117d8892c162430ff1c251422f937038bd92898a
Maik Blumenthal <m@element36.io>
2018-02-02 12:24:37 +0100

    renamed CHFT to CHF36

--- contracts/CHFT.sol
+++ contracts/CHFT.sol
@@ -7,11 +7,11 @@ import "zeppelin-solidity/contracts/token/StandardToken.sol";
 /// @author element36.io
 contract CHFT is StandardToken {
 
-    string public constant NAME = "CHF Token";
-    string public constant SYMBOL = "CHFT";
+    string public constant NAME = "CHF36 Token";
+    string public constant SYMBOL = "CHF36";
     uint8 public constant DECIMALS = 18;
 
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS));
+    uint256 public constant INITIAL_SUPPLY = 100000 * (10 ** uint256(DECIMALS));
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
