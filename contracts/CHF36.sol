53a9e354eeb963b185f8dc3fabd8da391c437ab9
Maik Blumenthal <m@element36.io>
2018-02-08 16:46:50 +0100

    rename to CHF36

--- contracts/CHF36.sol
+++ contracts/CHF36.sol
@@ -1,22 +1,22 @@
-pragma solidity ^0.4.15;
+pragma solidity 0.4.19;
 
 import "zeppelin-solidity/contracts/token/StandardToken.sol";
 
 
 /// @title CHFT Contract
 /// @author element36.io
-contract CHFT is StandardToken {
+contract CHF36 is StandardToken {
 
     string public constant NAME = "CHF36 Token";
     string public constant SYMBOL = "CHF36";
     uint8 public constant DECIMALS = 18;
 
-    uint256 public constant INITIAL_SUPPLY = 100000 * (10 ** uint256(DECIMALS));
+    uint256 public constant INITIAL_SUPPLY = 1000000 * (10 ** uint256(DECIMALS));
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    function CHFT() {
+    function CHF36() {
         totalSupply = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
     }
--- truffle.js
+++ truffle.js
@@ -2,23 +2,30 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-  networks: {
-    coverage: {
-      host: "localhost",
-      port: 8555,
-      network_id: "*",
-      gas: 0xfffffffffff,
-      gasPrice: 0x01 
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
     },
-    local_testrpc: {
-      host: "localhost",
-      port: 8558,
-      network_id: "85588558"
-    },
-    local_parity: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*"
+    networks: {
+        coverage: {
+            host: "localhost",
+            port: 8555,
+            network_id: "*",
+            gas: 0xfffffffffff,
+            gasPrice: 0x01
+        },
+        local_testrpc: {
+            host: "localhost",
+            port: 8558,
+            network_id: "85588558"
+        },
+        local_parity: {
+            host: "localhost",
+            port: 8545,
+            network_id: 10,
+            gas: 0x2fefd0
+        }
     }
-  }
 };
