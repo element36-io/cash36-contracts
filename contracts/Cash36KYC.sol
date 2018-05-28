06c78097a652a321493b624ad7470ba8f06eeff0
Maik Blumenthal <m@element36.io>
2018-05-28 11:13:09 +0200

    ci: fix build

--- contracts/Cash36KYC.sol
+++ contracts/Cash36KYC.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.4.21;
 
+
 contract Cash36KYC {
 
     address registry = 0xAcA1BCd8D0f5A9BFC95aFF331Da4c250CD9ac2Da;
@@ -15,7 +16,6 @@ contract Cash36KYC {
 
     function checkUser(address _user) public view returns(bool) {
         bool claim = registry.call(bytes4(keccak256("getClaim(address,address,bytes32)")), "2ozGXFqx3eKzmg7zQQZuTnEW6EeAVUzyUu6", _user, "cash36KYC");
-
-        return users[_user];
+        return claim;
     }
 }
