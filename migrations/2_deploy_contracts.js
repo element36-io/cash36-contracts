b4c204c352952f5d7413e8bd22e681358fa3f59b
Maik Blumenthal <m@element36.io>
2018-06-13 09:53:40 +0200

    ci: fix build

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,8 +6,8 @@ module.exports = function (deployer, network, accounts) {
     // On private network, we will deploy our own instance of
     // EthereumClaimsRegistry, otherwise we use the official one.
     if (network !== 1 || network !== 4) {
-        deployer.deploy(EthereumClaimsRegistry).then(() => {
-            deployer.deploy(Cash36KYC, EthereumClaimsRegistry.address, accounts[3]);
+        deployer.deploy(EthereumClaimsRegistry).then(function() {
+            return deployer.deploy(Cash36KYC, EthereumClaimsRegistry.address, accounts[3]);
         });
     } else {
         deployer.deploy(Cash36KYC, 0xAcA1BCd8D0f5A9BFC95aFF331Da4c250CD9ac2Da, 0x122bd1a75ae8c741f7e2ab0a28bd30b8dbb1a67e);
