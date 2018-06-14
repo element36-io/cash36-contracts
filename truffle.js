be16689e7a8ef779f6bce58f96e33ec5033a1b6c
Maik Blumenthal <m@element36.io>
2018-06-14 12:31:33 +0200

    ci: change pipeline

--- truffle.js
+++ truffle.js
@@ -16,24 +16,12 @@ module.exports = {
             gas: 0xfffffffffff,
             gasPrice: 0x01
         },
-        local_testrpc: {
+        local: {
             host: "localhost",
             port: 8558,
             network_id: "85588558",
             gas: 6721975,
             gasPrice: 20000000000,
-        },
-        local_parity: {
-            host: "localhost",
-            port: 8545,
-            network_id: 10,
-            gas: 0x2fefd0
-        },
-        local_dev: {
-            host: "localhost",
-            port: 8666,
-            network_id: 10,
-            gas: 0x2fefd5
         },
         dev: {
             host: "167.99.243.81",
