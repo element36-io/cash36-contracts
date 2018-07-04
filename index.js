3335cbb7c22c5f5ff643f5cc9c04051264924015
Maik Blumenthal <m@element36.io>
2018-07-04 12:37:24 +0200

    fix: index.js with new contracts

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 'use strict';
 
 var Cash36 = require('./build/contracts/Cash36');
-var SimpleKyc = require('./build/contracts/SimpleKYC');
 var Token36 = require('./build/contracts/Token36.json');
-var Token36Controller = require('./build/contracts/Token36Controller.json');
 
 module.exports = {
     Cash36Contract: Cash36,
-    SimpleKyc: SimpleKyc,
     Token36Contract: Token36,
-    Token36ControllerContract: Token36Controller,
 };
