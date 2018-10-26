b9c21bc462abd390f06383a5ee8fb3da53448afa
Maik Blumenthal <m@element36.io>
2018-10-26 09:32:03 +0200

    feat: add burnFrom and more tokens

--- contracts/GBP36/GBP36Controller.sol
+++ contracts/GBP36/GBP36Controller.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.24;
 import "./../Token36Controller.sol";
 
 
-/// @title USD36 Token Controller Contract
+/// @title GBP36 Token Controller Contract
 /// @author element36.io
-contract USD36Controller is Token36Controller {
+contract GBP36Controller is Token36Controller {
 
     // Constructor
     constructor(Token36 _token, address _compliance) public {
