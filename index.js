7113a14c9a910217b9305f5b2e53b34c479050de
Maik Blumenthal <m@element36.io>
2019-08-08 10:43:06 +0200

    add exports to index.js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 var Cash36 = require('./build/contracts/Cash36.json');
 var Cash36Compliance = require('./build/contracts/Cash36Compliance.json');
+var Cash36Exchanges = require('./build/contracts/Cash36Exchanges.json');
+var Cash36Company = require('./build/contracts/Cash36Company.json');
 var Token36 = require('./build/contracts/Token36.json');
 
 module.exports = {
     Cash36Contract: Cash36,
     Cash36ComplianceContract: Cash36Compliance,
+    Cash36ExchangesContract: Cash36Exchanges,
+    Cash36CompanyContract: Cash36Company,
     Token36Contract: Token36,
 };
