08347d90b4c0367db6bffcaaa7e19058a2676df0
Maik Blumenthal <m@element36.io>
2019-09-11 13:17:52 +0200

    new data for geth pkg

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,7 +11,7 @@ var Cash36Company = artifacts.require('./Cash36Company.sol')
 // var USD36Controller = artifacts.require('./USD36/USD36Controller.sol')
 // var USD36 = artifacts.require('./USD36/USD36.sol')
 
-module.exports = async (deployer, network) => {
+module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(Cash36Compliance)
   await deployer.deploy(Cash36Exchanges)
   await deployer.deploy(Cash36)
@@ -28,8 +28,8 @@ module.exports = async (deployer, network) => {
   await chf36Controller.transferOwnership(Cash36.address)
 
   let chf36 = await CHF36.deployed()
-  await chf36.addPauser(CHF36Controller.address);
-  await chf36.renouncePauser();
+  await chf36.addPauser(CHF36Controller.address)
+  await chf36.renouncePauser()
   await chf36.changeController(CHF36Controller.address)
 
   // Deploy EUR36
@@ -42,8 +42,8 @@ module.exports = async (deployer, network) => {
   await eur36Controller.transferOwnership(Cash36.address)
 
   let eur36 = await EUR36.deployed()
-  await eur36.addPauser(EUR36Controller.address);
-  await eur36.renouncePauser();
+  await eur36.addPauser(EUR36Controller.address)
+  await eur36.renouncePauser()
   await eur36.changeController(EUR36Controller.address)
 
   // Deploy GBP36
@@ -74,18 +74,42 @@ module.exports = async (deployer, network) => {
   let cash36Exchange = await Cash36Exchanges.deployed()
 
   // Deploy element36 as Company on Blockchain and whitelist
-  await deployer.deploy(Cash36Company, "element36 AG");
+  await deployer.deploy(Cash36Company, 'element36 AG')
   await cash36Compliance.addCompany(Cash36Company.address)
 
-  if (network == 'dev') {
+  if (network == 'local') {
     await cash36Exchange.addExchange('0x5c84e251671f94b5de719106fb34a1e99828d15d', CHF36.address)
     await cash36Exchange.addExchange('0x5c84e251671f94b5de719106fb34a1e99828d15d', EUR36.address)
 
     await cash36Compliance.changeOfficer('0xcd0dd78b1a09f860f39218d1124e121bf52d71a9')
+    await cash36.transferOwnership('0x9bed0471bd661a394793d4a9b11064da30b20ce5')
 
     // Current Owner Compliance Contract
-    let element36Company = await Cash36Company.deployed();
-    await element36Company.addOwner("0xcd0dd78b1a09f860f39218d1124e121bf52d71a9");
+    let element36Company = await Cash36Company.deployed()
+    await element36Company.addOwner('0xcd0dd78b1a09f860f39218d1124e121bf52d71a9')
+
+    console.log(await web3.eth.getBalance(accounts[0]));
+
+    await web3.eth.sendTransaction({
+      to: '0x5c84e251671f94b5de719106fb34a1e99828d15d',
+      from: accounts[0],
+      value: web3.utils.toWei('25', 'ether')
+    })
+    await web3.eth.sendTransaction({
+      to: '0xcd0dd78b1a09f860f39218d1124e121bf52d71a9',
+      from: accounts[0],
+      value: web3.utils.toWei('25', 'ether')
+    })
+    await web3.eth.sendTransaction({
+      to: '0x9bed0471bd661a394793d4a9b11064da30b20ce5',
+      from: accounts[0],
+      value: web3.utils.toWei('25', 'ether')
+    })
+
+    console.log(await web3.eth.getBalance('0x5c84e251671f94b5de719106fb34a1e99828d15d'));
+    console.log(await web3.eth.getBalance('0xcd0dd78b1a09f860f39218d1124e121bf52d71a9'));
+    console.log(await web3.eth.getBalance('0x9bed0471bd661a394793d4a9b11064da30b20ce5'));
+
   } else if (network == 'test') {
     await cash36Exchange.addExchange('0x5c84e251671f94b5de719106fb34a1e99828d15d', CHF36.address)
     await cash36Exchange.addExchange('0x5c84e251671f94b5de719106fb34a1e99828d15d', EUR36.address)
@@ -95,8 +119,8 @@ module.exports = async (deployer, network) => {
     await cash36Compliance.changeOfficer('0xcd0dd78b1a09f860f39218d1124e121bf52d71a9')
 
     // Current Owner Compliance Contract
-    let element36Company = await Cash36Company.deployed();
-    await element36Company.addOwner("0xcd0dd78b1a09f860f39218d1124e121bf52d71a9");
+    let element36Company = await Cash36Company.deployed()
+    await element36Company.addOwner('0xcd0dd78b1a09f860f39218d1124e121bf52d71a9')
   } else if (network == 'main') {
     await cash36Exchange.addExchange('0x5cf43737ccc03cfcb5af9841eaeb299f21f20003', CHF36.address)
     await cash36Exchange.addExchange('0x5cf43737ccc03cfcb5af9841eaeb299f21f20003', EUR36.address)
@@ -104,7 +128,7 @@ module.exports = async (deployer, network) => {
     await cash36Compliance.changeOfficer('0x502aac877a292c1f4b9c7f55359e72ff21f90002')
 
     // Current Owner Compliance Contract
-    let element36Company = await Cash36Company.deployed();
-    await element36Company.addOwner("0x502aac877a292c1f4b9c7f55359e72ff21f90002");
+    let element36Company = await Cash36Company.deployed()
+    await element36Company.addOwner('0x502aac877a292c1f4b9c7f55359e72ff21f90002')
   }
 }
